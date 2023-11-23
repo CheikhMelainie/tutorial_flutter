cbd366ec53017c56056f365e74b0f77ed5bb2832
CheikhMelainie <cheikh17767@gmail.com>
2023-11-23 15:50:13 +0100

    statfullless

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,14 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   int counter = 0;
   @override
   Widget build(BuildContext context) {
@@ -22,7 +29,9 @@ class MyApp extends StatelessWidget {
             Checkbox(
                 value: false,
                 onChanged: (value) {
-                  counter++;
+                  setState(() {
+                    counter++;
+                  });
                   print("Counter is => $counter");
                 }),
           ],
@@ -31,3 +40,30 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+// class MyApp extends StatelessWidget {
+//   int counter = 0;
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Welcom"),
+//         ),
+//         body: Column(
+//           children: <Widget>[
+//             Text(
+//               "Resut is $counter",
+//               style: TextStyle(fontSize: 22),
+//             ),
+//             Checkbox(
+//                 value: false,
+//                 onChanged: (value) {
+//                   counter++;
+//                   print("Counter is => $counter");
+//                 }),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
